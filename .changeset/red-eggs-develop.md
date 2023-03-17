44bcfad553be2bdbb33e24819b6fa222f5ed81a2
Daren Malfait <me@daren.be>
2023-03-17 08:46:15 +0100

    fix(combobox): add default field props

---
+++ .changeset/red-eggs-develop.md
@@ -0,0 +1,5 @@
+---
+"@nerdfish/ui": patch
+---
+
+add default field props to combobox
--- packages/ui/src/combobox.tsx
+++ packages/ui/src/combobox.tsx
@@ -3,27 +3,40 @@ import {cx} from '@nerdfish/utils'
 import {Check, ChevronsUpDown} from 'lucide-react'
 
 import {Command} from './command'
-import {getInputClassName} from './input'
+import {
+  InputError,
+  InputProps,
+  Label,
+  RawInputProps,
+  getInputClassName,
+} from './input'
 import {Popover} from './popover'
 
-const Combobox = React.forwardRef<
+interface ComboboxProps extends React.ComponentPropsWithoutRef<'input'> {
+  id?: string
+  items?: {
+    value: string
+    label: string
+  }[]
+  hasError?: boolean
+  placeholder?: string
+  emptyString?: string
+  searchDisabled?: boolean
+}
+
+const RawCombobox = React.forwardRef<
   HTMLInputElement,
-  React.ComponentPropsWithoutRef<'input'> & {
-    items?: {
-      value: string
-      label: string
-    }[]
-    placeholder?: string
-    emptyString?: string
-    searchDisabled?: boolean
-  }
->(function Combobox(
+  ComboboxProps & Pick<RawInputProps, 'hasError' | 'icon' | 'inputSize'>
+>(function RawCombobox(
   {
     className,
     placeholder = 'Search...',
     emptyString = 'No items found.',
     items = [],
     defaultValue = '',
+    hasError,
+    icon: Icon = ChevronsUpDown,
+    inputSize,
     searchDisabled,
     ...props
   },
@@ -41,7 +54,7 @@ const Combobox = React.forwardRef<
           aria-controls="listbox"
           aria-expanded={open}
           className={cx(
-            getInputClassName(),
+            getInputClassName(className, hasError, inputSize),
             'flex justify-between items-center',
             className,
           )}
@@ -57,10 +70,10 @@ const Combobox = React.forwardRef<
           {value
             ? items.find(item => item.value === value)?.label
             : placeholder}
-          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+          <Icon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </button>
       </Popover.Trigger>
-      <Popover.Content className="w-[200px] p-0">
+      <Popover.Content className="w-full min-w-[200px] p-0">
         <Command>
           {!searchDisabled ? (
             <>
@@ -92,6 +105,55 @@ const Combobox = React.forwardRef<
     </Popover>
   )
 })
+RawCombobox.displayName = 'RawCombobox'
+
+const Combobox = React.forwardRef<HTMLInputElement, ComboboxProps & InputProps>(
+  function Combobox(
+    {defaultValue, error, name, label, className, description, id, ...props},
+    ref,
+  ) {
+    const inputId = id ?? name
+    const errorId = `${inputId}-error`
+    const descriptionId = `${inputId}-description`
+
+    return (
+      <div className="w-full">
+        {label ? (
+          <div className="flex justify-between">
+            <Label htmlFor={inputId} className="mb-2">
+              {label}
+            </Label>
+            {description ? (
+              <span className="text-sm text-gray-200" id={descriptionId}>
+                {description}
+              </span>
+            ) : null}
+          </div>
+        ) : null}
+
+        <RawCombobox
+          ref={ref}
+          {...(props as ComboboxProps)}
+          name={name}
+          id={inputId}
+          autoComplete={name}
+          required
+          defaultValue={defaultValue}
+          aria-describedby={
+            error ? errorId : description ? descriptionId : undefined
+          }
+          hasError={!!error}
+        />
+
+        {error ? (
+          <InputError className="mt-2" id={errorId}>
+            {error}
+          </InputError>
+        ) : null}
+      </div>
+    )
+  },
+)
 Combobox.displayName = 'Combobox'
 
 export {Combobox}
