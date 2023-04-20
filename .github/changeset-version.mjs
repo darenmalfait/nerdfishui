58b90cb8400f80be93587b4e06e5df640127a3a8
Daren Malfait <github@daren.be>
2023-04-20 09:14:01 +0200

    ci(release): update version script

--- .github/changeset-version.mjs
+++ .github/changeset-version.mjs
@@ -9,4 +9,4 @@ import { exec } from "child_process"
 // This is a workaround until this is handled automatically by `changeset version`.
 // See https://github.com/changesets/changesets/issues/421.
 exec("npx changeset version")
-exec("npm i --package-lock-only")
+exec("pnpm install")
