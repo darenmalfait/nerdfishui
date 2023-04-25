59427cf9ea8f55a539b7de1724de2d6f32b5dfcd
Daren Malfait <github@daren.be>
2023-04-25 20:09:57 +0200

    fix(calendar): update styling

--- packages/ui/src/calendar.tsx
+++ packages/ui/src/calendar.tsx
@@ -3,7 +3,7 @@
 import * as React from 'react'
 import {cx} from '@nerdfish/utils'
 import {ChevronLeft, ChevronRight} from 'lucide-react'
-import {DayPicker} from 'react-day-picker'
+import {DayPicker, type DateRange} from 'react-day-picker'
 
 import {buttonVariants} from './button'
 
@@ -37,13 +37,14 @@ function Calendar({
           'text-gray-600 dark:text-gray-400 rounded-md w-9 font-normal text-[0.8rem]',
         row: 'flex w-full mt-2',
         cell: 'text-center text-sm p-0 relative [&:has([aria-selected])]:bg-secondary first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20',
-        day: 'h-9 w-9 p-0 font-normal aria-selected:opacity-100 rounded-full flex items-center justify-center hover:bg-secondary hover:text-secondary hover:scale-105 active:scale-100 transition-transform',
-        day_selected: 'bg-inverse text-inverse',
-        day_today: 'bg-nerdfish-500 text-white',
+        day: 'h-9 w-9 p-0 font-normal aria-selected:opacity-100 rounded-full flex items-center justify-center hover:bg-inverse hover:text-inverse hover:scale-105 active:scale-100 transition-transform',
+        day_selected: '!bg-inverse !text-inverse',
+        day_today:
+          'bg-nerdfish-500 text-white hover:!bg-nerdfish-500 hover:!text-white',
         day_outside: 'text-gray-400 dark:text-gray-600 opacity-50',
         day_disabled: 'text-gray-400 dark:text-gray-600 opacity-50',
         day_range_middle:
-          'aria-selected:bg-gray-400 aria-selected:text-primary',
+          'aria-selected:bg-secondary aria-selected:text-secondary',
         day_hidden: 'invisible',
         ...classNames,
       }}
@@ -58,4 +59,4 @@ function Calendar({
 }
 Calendar.displayName = 'Calendar'
 
-export {Calendar}
+export {Calendar, type DateRange}
--- packages/ui/src/popover.tsx
+++ packages/ui/src/popover.tsx
@@ -14,7 +14,7 @@ const PopoverContent = React.forwardRef<
       align={align}
       sideOffset={sideOffset}
       className={cx(
-        'animate-in data-[side=bottom]:slide-in-from-top-2 data-[side=top]:slide-in-from-bottom-2 data-[side=right]:slide-in-from-left-2 data-[side=left]:slide-in-from-right-2 z-50 w-72 rounded-md border border-gray-100 bg-white p-4 shadow-md outline-none dark:border-gray-800 dark:bg-gray-800',
+        'animate-in data-[side=bottom]:slide-in-from-top-2 data-[side=top]:slide-in-from-bottom-2 data-[side=right]:slide-in-from-left-2 data-[side=left]:slide-in-from-right-2 z-50 w-72 rounded-md shadow-outline bg-white p-4 shadow-md outline-none dark:bg-gray-900',
         className,
       )}
       {...props}
