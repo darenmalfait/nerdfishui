f3715fcf369a87b4f4d58a0ed1d40cbc9a9b564c
Daren Malfait <github@daren.be>
2023-08-14 15:09:30 +0200

    fix(popover): remove portal from component

---
+++ .changeset/dull-ants-stare.md
@@ -0,0 +1,5 @@
+---
+"@nerdfish/ui": patch
+---
+
+fix(popover): remove portal form popover
--- packages/ui/src/popover.tsx
+++ packages/ui/src/popover.tsx
@@ -8,18 +8,16 @@ const PopoverContent = React.forwardRef<
   React.ElementRef<typeof PopoverPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
 >(({className, align = 'center', sideOffset = 4, ...props}, ref) => (
-  <PopoverPrimitive.Portal>
-    <PopoverPrimitive.Content
-      ref={ref}
-      align={align}
-      sideOffset={sideOffset}
-      className={cx(
-        'animate-in data-[side=bottom]:slide-in-from-top-2 data-[side=top]:slide-in-from-bottom-2 data-[side=right]:slide-in-from-left-2 data-[side=left]:slide-in-from-right-2 z-50 w-72 rounded-md shadow-outline bg-white p-4 shadow-md outline-none dark:bg-gray-900',
-        className,
-      )}
-      {...props}
-    />
-  </PopoverPrimitive.Portal>
+  <PopoverPrimitive.Content
+    ref={ref}
+    align={align}
+    sideOffset={sideOffset}
+    className={cx(
+      'animate-in data-[side=bottom]:slide-in-from-top-2 data-[side=top]:slide-in-from-bottom-2 data-[side=right]:slide-in-from-left-2 data-[side=left]:slide-in-from-right-2 z-50 w-72 rounded-md shadow-outline bg-white p-4 shadow-md outline-none dark:bg-gray-900',
+      className,
+    )}
+    {...props}
+  />
 ))
 PopoverContent.displayName = PopoverPrimitive.Content.displayName
 
