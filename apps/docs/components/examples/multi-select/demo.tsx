1be63c7d30429c53db14907cd7f9fab95897e9c6
Daren Malfait <github@daren.be>
2023-08-28 19:33:45 +0200

    feat(multi-select): improve badge design and functionality

--- apps/docs/components/examples/multi-select/demo.tsx
+++ apps/docs/components/examples/multi-select/demo.tsx
@@ -9,12 +9,18 @@ export function MultiSelectDemo() {
       name="framework"
       label="Framework"
       placeholder="Select a framework"
+      onCreateItemsClicked={(value: string) => {
+        console.info(`Create item "${value}"`)
+      }}
+      onEditItemsClicked={() => {
+        console.info('Edit items')
+      }}
       items={[
-        {value: 'next.js', label: 'Next.js'},
-        {value: 'sveltekit', label: 'SvelteKit'},
-        {value: 'nuxt.js', label: 'Nuxt.js'},
-        {value: 'remix', label: 'Remix'},
-        {value: 'astro', label: 'Astro'},
+        {value: 'next.js', label: 'Next.js', color: '#000000'},
+        {value: 'sveltekit', label: 'SvelteKit', color: '#ff3e00'},
+        {value: 'nuxt.js', label: 'Nuxt.js', color: '#00c58e'},
+        {value: 'remix', label: 'Remix', color: '#f5f5f5'},
+        {value: 'astro', label: 'Astro', color: '#ffffff'},
       ]}
     />
   )
--- packages/ui/src/multi-select.tsx
+++ packages/ui/src/multi-select.tsx
@@ -1,19 +1,13 @@
 'use client'
 
 import * as React from 'react'
-import {cx, ExtractProps, useControllableState} from '@nerdfish/utils'
-import {DialogClose} from '@radix-ui/react-dialog'
-import {Check, ChevronsUpDown, Edit2} from 'lucide-react'
+import {cx, getContrastColor, useControllableState} from '@nerdfish/utils'
+import {Check, ChevronsUpDown, Pencil, Plus} from 'lucide-react'
 
-import {Accordion} from './accordion'
-import {AlertDialog} from './alert-dialog'
 import {Badge} from './badge'
-import {Button} from './button'
 import {Command} from './command'
-import {Dialog} from './dialog'
 import {
   getInputClassName,
-  Input,
   InputError,
   InputProps,
   Label,
@@ -26,189 +20,14 @@ type Item = {
   value: string
   label: string
   color?: string
+  description?: string
 }
 
 const badgeStyle = (color: string) => ({
-  borderColor: `${color}20`,
-  backgroundColor: `${color}30`,
-  color,
+  borderColor: color,
+  backgroundColor: color,
+  color: getContrastColor(color),
 })
-
-type DialogLabelProps = {
-  itemDeleteLabel?: string
-  itemDeleteConfirmationTitleLabel?: string
-  itemDeleteConfirmationDescriptionLabel?: string
-  itemEditLabel?: string
-  itemCancelLabel?: string
-  itemSaveLabel?: string
-  // eslint-disable-next-line react/no-unused-prop-types
-  itemsEditLabel?: string
-  // eslint-disable-next-line react/no-unused-prop-types
-  itemsEditDescriptionLabel?: string
-  // eslint-disable-next-line react/no-unused-prop-types
-  itemsCreateLabel?: string
-}
-
-const defaultLabels: DialogLabelProps = {
-  itemDeleteLabel: 'Delete',
-  itemDeleteConfirmationTitleLabel: 'Are you sure?',
-  itemDeleteConfirmationDescriptionLabel: 'You are about to delete the label',
-  itemEditLabel: 'Edit',
-  itemCancelLabel: 'Cancel',
-  itemSaveLabel: 'Save',
-  itemsEditLabel: 'Edit',
-  itemsEditDescriptionLabel: 'Edit the labels',
-  itemsCreateLabel: 'Create new item',
-}
-
-const CommandItemCreate = ({
-  inputValue,
-  items,
-  onSelect,
-  itemsCreateLabel = defaultLabels.itemsCreateLabel,
-}: {
-  inputValue: string
-  items: Item[]
-  onSelect: () => void
-} & Pick<DialogLabelProps, 'itemsCreateLabel'>) => {
-  const hasNoItems = !items
-    .map(({value}) => value)
-    .includes(`${inputValue.toLowerCase()}`)
-
-  const render = inputValue !== '' && hasNoItems
-
-  if (!render) return null
-
-  // BUG: whenever a space is appended, the Create-Button will not be shown.
-  return (
-    <Command.Item
-      key={`${inputValue}`}
-      value={`${inputValue}`}
-      className="text-secondary text-xs"
-      onSelect={onSelect}
-    >
-      <div className={cx('mr-2 h-4 w-4')} />
-      {itemsCreateLabel} &quot;{inputValue}&quot;
-    </Command.Item>
-  )
-}
-
-const DialogListItem = ({
-  value,
-  label,
-  color,
-  onSubmit,
-  onDelete,
-  itemDeleteLabel = defaultLabels.itemDeleteLabel,
-  itemDeleteConfirmationTitleLabel = defaultLabels.itemDeleteConfirmationTitleLabel,
-  itemDeleteConfirmationDescriptionLabel = defaultLabels.itemDeleteConfirmationDescriptionLabel,
-  itemEditLabel = defaultLabels.itemEditLabel,
-  itemCancelLabel = defaultLabels.itemCancelLabel,
-  itemSaveLabel = defaultLabels.itemSaveLabel,
-}: Item & {
-  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void
-  onDelete: () => void
-} & DialogLabelProps) => {
-  const inputRef = React.useRef<HTMLInputElement>(null)
-  const [accordionValue, setAccordionValue] = React.useState<string>('')
-  const [inputValue, setInputValue] = React.useState<string>(label)
-  const [colorValue, setColorValue] = React.useState<string>(color ?? '#ffffff')
-  const disabled = label === inputValue && color === colorValue
-
-  React.useEffect(() => {
-    if (accordionValue !== '') {
-      inputRef.current?.focus()
-    }
-  }, [accordionValue])
-
-  return (
-    <Accordion
-      key={value}
-      type="single"
-      collapsible
-      value={accordionValue}
-      onValueChange={setAccordionValue}
-    >
-      <Accordion.Item value={value}>
-        <div className="flex items-center justify-between">
-          <div>
-            <Badge variant="outline" style={badgeStyle(color ?? '#ffffff')}>
-              {label}
-            </Badge>
-          </div>
-          <div className="flex items-center gap-4">
-            <Accordion.Trigger>{itemEditLabel}</Accordion.Trigger>
-            <AlertDialog>
-              <AlertDialog.Trigger asChild>
-                <Button variant="danger" size="sm">
-                  {itemDeleteLabel}
-                </Button>
-              </AlertDialog.Trigger>
-              <AlertDialog.Content>
-                <AlertDialog.Header>
-                  <AlertDialog.Title>
-                    {itemDeleteConfirmationTitleLabel}
-                  </AlertDialog.Title>
-                  <AlertDialog.Description>
-                    {itemDeleteConfirmationDescriptionLabel}{' '}
-                    <Badge
-                      variant="outline"
-                      style={badgeStyle(color ?? '#ffffff')}
-                    >
-                      {label}
-                    </Badge>{' '}
-                    .
-                  </AlertDialog.Description>
-                </AlertDialog.Header>
-                <AlertDialog.Footer>
-                  <AlertDialog.Cancel>{itemCancelLabel}</AlertDialog.Cancel>
-                  <AlertDialog.Action onClick={onDelete}>
-                    {itemDeleteLabel}
-                  </AlertDialog.Action>
-                </AlertDialog.Footer>
-              </AlertDialog.Content>
-            </AlertDialog>
-          </div>
-        </div>
-        <Accordion.Content>
-          <form
-            className="flex items-end gap-4"
-            onSubmit={e => {
-              onSubmit(e)
-              setAccordionValue('')
-            }}
-          >
-            <div className="grid w-full gap-3">
-              <Input
-                name="name"
-                label="Label name"
-                ref={inputRef}
-                id="name"
-                value={inputValue}
-                onChange={e => setInputValue(e.target.value)}
-                className="h-8"
-              />
-            </div>
-            <div className="grid gap-3">
-              <Input
-                name="color"
-                label="Color"
-                id="color"
-                type="color"
-                value={colorValue}
-                onChange={e => setColorValue(e.target.value)}
-                className="h-8 px-2 py-1"
-              />
-            </div>
-            <Button type="submit" disabled={disabled} size="sm">
-              {itemSaveLabel}
-            </Button>
-          </form>
-        </Accordion.Content>
-      </Accordion.Item>
-    </Accordion>
-  )
-}
 
 type MultiSelectProps = Omit<
   React.ComponentPropsWithoutRef<'input'>,
@@ -216,20 +35,14 @@ type MultiSelectProps = Omit<
 > &
   Pick<RawInputProps, 'hasError' | 'icon' | 'inputSize'> & {
     items?: Item[]
-    id?: string
     placeholder?: string
     onChange?: React.Dispatch<React.SetStateAction<Item[] | undefined>>
     value?: Item[]
     defaultValue?: Item[]
-    editDisabled?: boolean
-    createDisabled?: boolean
-    onCreateItem?: (item: Item) => void
-    onDeleteItem?: (item: Item) => void
-    onUpdateItem?: (item: Item) => void
-    customEditListItem?: (
-      props: ExtractProps<typeof DialogListItem>,
-    ) => React.ReactElement
-    labels?: DialogLabelProps
+    onCreateItemsClicked?: (value: string) => void
+    onEditItemsClicked?: () => void
+    createNewLabel?: string
+    editItemsLabel?: string
   }
 
 const RawMultiSelect = React.forwardRef<HTMLInputElement, MultiSelectProps>(
@@ -238,53 +51,25 @@ const RawMultiSelect = React.forwardRef<HTMLInputElement, MultiSelectProps>(
       value: valueProp,
       defaultValue,
       onChange,
-      items: itemsProp,
-      onCreateItem,
-      onDeleteItem,
+      items = [],
+      onCreateItemsClicked,
+      onEditItemsClicked,
       hasError,
-      onUpdateItem,
-      editDisabled = true,
-      createDisabled = true,
       placeholder = 'Select items',
-      customEditListItem: CustomEditListItem,
-      labels: labelsProp,
+      createNewLabel,
+      editItemsLabel,
       ...props
     },
     ref,
   ) {
-    const labels = {...defaultLabels, ...labelsProp}
-    const {
-      itemsEditLabel = defaultLabels.itemsEditLabel,
-      itemsEditDescriptionLabel = defaultLabels.itemsEditDescriptionLabel,
-    } = labels
     const inputRef = React.useRef<HTMLInputElement>(null)
-    const [items, setItems] = React.useState<Item[]>(itemsProp ?? [])
     const [openCombobox, setOpenCombobox] = React.useState(false)
-    const [openDialog, setOpenDialog] = React.useState(false)
     const [inputValue, setInputValue] = React.useState<string>('')
     const [selectedValues = [], setSelectedValues] = useControllableState<
       Item[]
     >(valueProp, defaultValue ?? [], onChange)
 
     React.useImperativeHandle(ref, () => inputRef.current as HTMLInputElement)
-
-    function createItem(name: string) {
-      const newItem = {
-        value: name.toLowerCase(),
-        label: name,
-        color: '#ffffff',
-      }
-
-      onCreateItem?.(newItem)
-      setItems([...items, newItem])
-      setSelectedValues([...selectedValues, newItem])
-    }
-
-    function updateItem(item: Item, newItem: Item) {
-      onUpdateItem?.(newItem)
-      setItems(items.map(f => (f.value === item.value ? newItem : f)))
-      setSelectedValues(items.map(f => (f.value === item.value ? newItem : f)))
-    }
 
     function toggleItem(item: Item) {
       setSelectedValues(
@@ -293,12 +78,6 @@ const RawMultiSelect = React.forwardRef<HTMLInputElement, MultiSelectProps>(
           : selectedValues.filter(l => l.value !== item.value),
       )
       inputRef.current?.focus()
-    }
-
-    function deleteItem(item: Item) {
-      onDeleteItem?.(item)
-      setItems(items.filter(f => f.value !== item.value))
-      setSelectedValues(selectedValues.filter(f => f.value !== item.value))
     }
 
     function onComboboxOpenChange(value: boolean) {
@@ -348,115 +127,83 @@ const RawMultiSelect = React.forwardRef<HTMLInputElement, MultiSelectProps>(
                 value={inputValue}
                 onValueChange={setInputValue}
               />
-              <Command.Group>
-                <ScrollArea className="h-32 w-full">
+              <ScrollArea className="h-32 w-full">
+                <Command.Group>
                   {items.map(item => {
                     const isActive = selectedValues.includes(item)
+
                     return (
                       <Command.Item
                         key={item.value}
                         value={item.value}
                         onSelect={() => toggleItem(item)}
+                        className="items-start"
                       >
                         <Check
                           className={cx(
-                            'mr-2 h-4 w-4',
+                            'mr-2 h-4 w-4 pt-1',
                             isActive ? 'opacity-100' : 'opacity-0',
                           )}
                         />
-                        <div className="flex-1">{item.label}</div>
-                        <div
-                          className="h-4 w-4 rounded-full"
-                          style={{backgroundColor: item.color}}
-                        />
+                        <div className="flex flex-col items-start gap-y-1">
+                          <div className="flex justify-start">
+                            <span
+                              className="mr-2 h-4 w-4 rounded-full"
+                              style={{backgroundColor: item.color}}
+                            />
+                            <span className="flex-1">{item.label}</span>
+                          </div>
+                          {item.description ? (
+                            <span className="text-secondary text-[10px]">
+                              {item.description}
+                            </span>
+                          ) : null}
+                        </div>
                       </Command.Item>
                     )
                   })}
-                  {!createDisabled ? (
-                    <CommandItemCreate
-                      {...labels}
-                      onSelect={() => createItem(inputValue)}
-                      {...{inputValue, items}}
-                    />
-                  ) : null}
-                </ScrollArea>
-              </Command.Group>
-              {!editDisabled ? (
-                <>
-                  <Command.Separator alwaysRender />
-                  <Command.Group>
+                  {onEditItemsClicked && inputValue.length === 0 ? (
                     <Command.Item
-                      value={`:${inputValue}:`} // HACK: that way, the edit button will always be shown
-                      className="text-secondary text-xs"
-                      onSelect={() => setOpenDialog(true)}
+                      onSelect={() => {
+                        setOpenCombobox(false)
+                        onEditItemsClicked()
+                      }}
+                      className="flex gap-x-2"
+                      {...{inputValue, items}}
                     >
-                      <div className={cx('mr-2 h-4 w-4')} />
-                      <Edit2 className="mr-2 h-2.5 w-2.5" />
-                      {itemsEditLabel}
+                      <span>
+                        <Pencil className="h-4 w-4" />
+                      </span>
+                      <span>{editItemsLabel ?? 'Edit labels'}</span>
                     </Command.Item>
-                  </Command.Group>
-                </>
-              ) : null}
+                  ) : null}
+                </Command.Group>
+                {onCreateItemsClicked && inputValue.length > 0 ? (
+                  <button
+                    onSelect={() => {
+                      setOpenCombobox(false)
+                      onCreateItemsClicked(inputValue)
+                    }}
+                    className="flex w-full items-center gap-x-2 px-2 py-1"
+                  >
+                    <span>
+                      <Plus className="h-4 w-4" />
+                    </span>
+                    <span>{createNewLabel ?? `Create "${inputValue}"`}</span>
+                  </button>
+                ) : null}
+              </ScrollArea>
             </Command>
           </Popover.Content>
         </Popover>
-        {!editDisabled ? (
-          <Dialog
-            open={openDialog}
-            onOpenChange={open => {
-              if (!open) {
-                setOpenCombobox(true)
-              }
-              setOpenDialog(open)
-            }}
-          >
-            <Dialog.Content className="flex max-h-[90vh] flex-col">
-              <Dialog.Header>
-                <Dialog.Title>{itemsEditLabel}</Dialog.Title>
-                <Dialog.Description>
-                  {itemsEditDescriptionLabel}
-                </Dialog.Description>
-              </Dialog.Header>
-              <ScrollArea className="-mx-6 h-32 w-full flex-1 px-6 py-2">
-                {items.map(item => {
-                  const ListItem = CustomEditListItem ?? DialogListItem
 
-                  return (
-                    <ListItem
-                      key={item.value}
-                      {...labels}
-                      onDelete={() => deleteItem(item)}
-                      onSubmit={e => {
-                        e.preventDefault()
-                        const target = e.target as typeof e.target &
-                          Record<'name' | 'color', {value: string}>
-                        const newItem = {
-                          value: target.name.value.toLowerCase(),
-                          label: target.name.value,
-                          color: target.color.value,
-                        }
-                        updateItem(item, newItem)
-                      }}
-                      {...item}
-                    />
-                  )
-                })}
-              </ScrollArea>
-              <Dialog.Footer className="bg-secondary/40">
-                <DialogClose asChild>
-                  <Button variant="outline">Close</Button>
-                </DialogClose>
-              </Dialog.Footer>
-            </Dialog.Content>
-          </Dialog>
-        ) : null}
         <div className="relative mt-3 overflow-y-auto">
           {selectedValues.map(({label, value, color}) => (
             <Badge
               key={value}
               variant="outline"
               style={color ? badgeStyle(color) : undefined}
-              className="mb-2 mr-2"
+              className="dark:!bg-opacity/20 bg-secondary shadow-outline mb-2 mr-2 border"
             >
               {label}
             </Badge>
