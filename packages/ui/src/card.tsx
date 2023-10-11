05fc89983580d09fcb9d2d33e0150162725beab8
Daren Malfait <daren@nerdfish.be>
2023-10-11 14:35:51 +0200

    style(card): change border style

--- packages/ui/src/card.tsx
+++ packages/ui/src/card.tsx
@@ -8,7 +8,7 @@ const CardRoot = React.forwardRef<
   <div
     ref={ref}
     className={cx(
-      'relative flex flex-col min-w-0 mb-6 break-words bg-primary border-0 shadow-outline shadow-soft-sm rounded-md',
+      'relative flex flex-col min-w-0 mb-6 break-words bg-primary border-0 shadow-outline rounded-md',
       className,
     )}
     {...props}
@@ -23,7 +23,7 @@ const CardHeader = React.forwardRef<
   <div
     ref={ref}
     className={cx(
-      'p-6 pb-2 mb-0 bg-primary flex flex-col space-y-2 border-b-0 border-b-solid rounded-t-2xl border-b-transparent',
+      'p-6 pb-2 mb-0 bg-primary flex flex-col space-y-2 border-b-0 border-b-solid rounded-t-md border-b-transparent',
       className,
     )}
     {...props}
