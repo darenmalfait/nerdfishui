f15a4731532f7264bccdbfb165c918d34bfa619e
Daren Malfait <daren@nerdfish.be>
2023-10-15 16:11:14 +0200

    style(button): add press animation

---
+++ .changeset/clever-spiders-nail.md
@@ -0,0 +1,5 @@
+---
+"@nerdfish/ui": patch
+---
+
+add button press animation
--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -5,7 +5,7 @@ import {cva, cx, VariantProps} from '@nerdfish/utils'
 import {Slot} from '@radix-ui/react-slot'
 
 const buttonVariants = cva(
-  'ring-offset-background focus-visible:ring-ring inline-flex items-center justify-center rounded-md text-sm font-medium transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'ring-offset-background focus-visible:ring-ring inline-flex items-center justify-center rounded-md text-sm font-medium transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 active:scale-90 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
