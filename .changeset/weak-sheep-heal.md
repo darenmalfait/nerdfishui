78b60b6876743623e52d65e49e8abdd842c08919
Daren Malfait <daren@nerdfish.be>
2024-01-18 19:37:50 +0100

    fix(tabs): adjust background color in dark mode

---
+++ .changeset/weak-sheep-heal.md
@@ -0,0 +1,5 @@
+---
+"@nerdfish/ui": patch
+---
+
+fix background color for Tabs in dark mode
--- packages/ui/src/tabs.tsx
+++ packages/ui/src/tabs.tsx
@@ -25,7 +25,7 @@ const TabsTrigger = React.forwardRef<
 >(({className, ...props}, ref) => (
   <TabsPrimivite.Trigger
     className={cx(
-      'inline-flex min-w-[100px] items-center justify-center rounded-[0.185rem] px-3 py-1.5  text-sm font-medium text-gray-700 transition-all  disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-white data-[state=active]:text-gray-900 data-[state=active]:shadow-sm dark:text-gray-200 dark:data-[state=active]:bg-gray-900',
+      'inline-flex min-w-[100px] items-center justify-center rounded-[0.185rem] px-3 py-1.5  text-sm font-medium text-gray-700 transition-all  disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-white data-[state=active]:text-gray-900 data-[state=active]:shadow-sm dark:text-gray-200 dark:data-[state=active]:text-gray-900 dark:data-[state=active]:bg-gray-100',
       className,
     )}
     {...props}
