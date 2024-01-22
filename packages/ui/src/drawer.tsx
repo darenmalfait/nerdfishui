17b4b1c94bfc4abc33169817918b5969984e1223
Daren Malfait <daren@nerdfish.be>
2024-01-22 21:23:16 +0100

    fix(drawer): change bg color to primary

--- packages/ui/src/drawer.tsx
+++ packages/ui/src/drawer.tsx
@@ -42,7 +42,7 @@ const DrawerContent = React.forwardRef<
       )}
       {...props}
     >
-      <div className="bg-muted mx-auto mt-4 h-2 w-[100px] rounded-full" />
+      <div className="bg-primary mx-auto mt-4 h-2 w-[100px] rounded-full" />
       {children}
     </DrawerPrimitive.Content>
   </DrawerPrimitive.Portal>
