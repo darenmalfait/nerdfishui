f254f0b8e96e79349284a61fbb3c32e7f75fbbc4
Daren Malfait <daren@nerdfish.be>
2024-01-24 18:03:54 +0100

    docs(input): add Input field

---
+++ apps/docs/components/examples/date-picker/with-input.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import * as React from 'react'
+import {DatePicker, Input} from '@nerdfish/ui'
+import {addYears, format} from 'date-fns'
+import {Calendar} from 'lucide-react'
+
+export function DatePickerWithInputExample() {
+  const [date, setDate] = React.useState<Date>(new Date())
+
+  return (
+    <div className="flex w-full gap-2">
+      <DatePicker
+        className="p-3"
+        selected={date}
+        onSelect={setDate}
+        fromYear={addYears(new Date(), -120).getFullYear()}
+        toYear={new Date().getFullYear()}
+      >
+        <Input
+          name="date"
+          label="Date"
+          placeholder="Date"
+          icon={Calendar}
+          value={format(date, 'yyyy-MM-dd')}
+          onChange={e => setDate(new Date(e.target.value))}
+          description="Your date of birth"
+        />
+      </DatePicker>
+    </div>
+  )
+}
