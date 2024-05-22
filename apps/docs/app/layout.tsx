ce191fb9b9770ace78a610939c7dce7195888ff9
Daren Malfait <daren@nerdfish.be>
2024-05-22 07:30:21 +0200

    chore(deps): update packages

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -1,17 +1,11 @@
 import * as React from 'react'
-import {Inter as fontSans} from 'next/font/google'
+import {GeistSans} from 'geist/font/sans'
 
 import '@nerdfish/theme/dist/nerdfishui.css'
 import '../styles/global.css'
 
 import {Layout} from '../components/layout'
 import {AppProviders} from '../context/app-providers'
-
-const sans = fontSans({
-  subsets: ['latin'],
-  variable: '--font-sans',
-  display: 'swap',
-})
 
 interface RootLayoutProps {
   children: React.ReactNode
@@ -22,10 +16,10 @@ export default function RootLayout({children}: RootLayoutProps) {
     <html lang="en" suppressHydrationWarning>
       <head />
       <body
-        className={`min-h-screen bg-primary font-sans antialiased ${sans.variable}`}
+        className={`min-h-screen bg-primary font-sans antialiased ${GeistSans.variable}`}
       >
         <AppProviders>
-          <main className={`${sans.variable} bg-primary font-sans`}>
+          <main className={`${GeistSans.variable} bg-primary font-sans`}>
             <Layout>{children}</Layout>
           </main>
         </AppProviders>
