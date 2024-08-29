a76c7d0c054ce9fa3ac6b9efe87a724bf3b94f89
Daren Malfait <daren@nerdfish.be>
2024-08-29 17:22:00 +0200

    fix(docs): adjust tailwind config

---
+++ apps/docs/tailwind.config.ts
@@ -0,0 +1,36 @@
+const { fontFamily } =
+	// eslint-disable-next-line
+	require('tailwindcss/defaultTheme') as typeof import('tailwindcss/defaultTheme')
+
+module.exports = {
+	content: [
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+		'src/**/*.{ts,tsx}',
+		'../../node_modules/@nerdfish/**/*.{js,ts,jsx,tsx}',
+		'../../packages/ui/src/**/*.{ts,tsx}',
+	],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			fontFamily: {
+				title: ['var(--font-geist-sans)', ...fontFamily.sans],
+				sans: ['var(--font-geist-sans)', ...fontFamily.sans],
+			},
+		},
+	},
+	typography: (theme: any) => ({
+		DEFAULT: {
+			css: {
+				h1: {
+					...theme('fontSize.2xl')[1],
+					marginBottom: theme('spacing.2'),
+				},
+			},
+		},
+	}),
+	plugins: [
+		require('tailwindcss-animate'),
+		require('@tailwindcss/typography'),
+		require('@nerdfish/tailwind-config'),
+	],
+}
