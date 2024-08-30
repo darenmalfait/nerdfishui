982fe5bb27d902c907e2eea873dbce7acc297af3
Daren Malfait <daren@nerdfish.be>
2024-08-30 08:49:42 +0200

    refactor(link): adjust exports

--- packages/ui/src/link.tsx
+++ packages/ui/src/link.tsx
@@ -3,51 +3,50 @@
 import { cx } from '@nerdfish/utils'
 import * as React from 'react'
 
-type LinkProps = React.DetailedHTMLProps<
-	React.AnchorHTMLAttributes<HTMLAnchorElement>,
-	HTMLAnchorElement
-> & {
-	disabled?: boolean
-	external?: boolean
-	as?: React.ElementType
-	onClick?: React.MouseEventHandler<HTMLAnchorElement>
-	to?: string
-	children?: React.ReactNode
-}
+export const Link = React.forwardRef<
+	HTMLElement,
+	React.DetailedHTMLProps<
+		React.AnchorHTMLAttributes<HTMLAnchorElement>,
+		HTMLAnchorElement
+	> & {
+		disabled?: boolean
+		external?: boolean
+		as?: React.ElementType
+		onClick?: React.MouseEventHandler<HTMLAnchorElement>
+		to?: string
+		children?: React.ReactNode
+	}
+>(function Link(props, ref) {
+	const {
+		disabled,
+		external,
+		onClick,
+		to,
+		href = to,
+		className,
+		as: Tag = 'a',
+		...rest
+	} = props
+	const externalProps = external
+		? { target: '_blank', rel: 'noopener noreferrer' }
+		: null
 
-const Link = React.forwardRef<HTMLElement, LinkProps>(
-	function Link(props, ref) {
-		const {
-			disabled,
-			external,
-			onClick,
-			to,
-			href = to,
-			className,
-			as: Tag = 'a',
-			...rest
-		} = props
-		const externalProps = external
-			? { target: '_blank', rel: 'noopener noreferrer' }
-			: null
+	return (
+		<Tag
+			ref={ref}
+			className={cx(
+				'text-primary inline-flex space-x-2 bg-transparent hover:bg-transparent dark:hover:bg-transparent',
+				className,
+			)}
+			tabIndex={disabled ? -1 : undefined}
+			to={href}
+			href={href}
+			aria-disabled={disabled}
+			onClick={disabled ? (event: any) => event.preventDefault() : onClick}
+			{...externalProps}
+			{...rest}
+		/>
+	)
+})
 
-		return (
-			<Tag
-				ref={ref}
-				className={cx(
-					'text-primary inline-flex space-x-2 bg-transparent hover:bg-transparent dark:hover:bg-transparent',
-					className,
-				)}
-				tabIndex={disabled ? -1 : undefined}
-				to={href}
-				href={href}
-				aria-disabled={disabled}
-				onClick={disabled ? (event: any) => event.preventDefault() : onClick}
-				{...externalProps}
-				{...rest}
-			/>
-		)
-	},
-)
-
-export { Link, type LinkProps }
+export type LinkProps = React.ComponentPropsWithoutRef<typeof Link>
