ff40fa1180488c7574a868683c5045b2a1fb471b
Daren Malfait <daren@nerdfish.be>
2024-09-06 16:01:29 +0200

    feat(ui): add new sidebar component

---
+++ .changeset/warm-stingrays-perform.md
@@ -0,0 +1,5 @@
+---
+'@nerdfish/ui': minor
+---
+
+add `Sidebar` component
--- apps/docs/src/components/layout.tsx
+++ apps/docs/src/components/layout.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { ScrollArea, Toaster } from '@nerdfish/ui'
+import { Toaster } from '@nerdfish/ui'
 import * as React from 'react'
 
 import { Footer } from './footer'
@@ -15,11 +15,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
 
 			<div className="container mx-auto px-4">
 				<div className="flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
-					<aside className="border-muted fixed top-14 z-30 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 overflow-y-auto border-r md:sticky md:block">
-						<ScrollArea className="pr-6 lg:py-10">
-							<Navigation />
-						</ScrollArea>
-					</aside>
+					<Navigation className="hidden md:block" />
 					<div className="py-6">
 						<Prose as="article">{children}</Prose>
 						<Footer />
