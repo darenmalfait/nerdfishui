bab4829f3daeed2eda482632f6939ec501cf4eab
Daren Malfait <daren@nerdfish.be>
2024-09-06 11:05:04 +0200

    feat(input): add shake animation on error

--- apps/docs/src/components/examples/input/demo.tsx
+++ apps/docs/src/components/examples/input/demo.tsx
@@ -1,15 +1,44 @@
 'use client'
 
-import { Input } from '@nerdfish/ui'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Button, Input, toast } from '@nerdfish/ui'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+const validationSchema = z.object({
+	email: z.string().email(),
+})
+
+type FormData = z.infer<typeof validationSchema>
 
 export function InputDemo() {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<FormData>({
+		resolver: zodResolver(validationSchema),
+	})
+
+	function onSubmit(data: FormData) {
+		toast.success(`Form submitted successfully: ${data.email}`)
+	}
+
 	return (
-		<Input
-			name="email"
-			label="email"
-			description="your login name"
-			type="email"
-			placeholder="Email"
-		/>
+		<form
+			onSubmit={handleSubmit(onSubmit)}
+			className="flex w-full flex-col gap-4"
+			noValidate
+		>
+			<Input
+				{...register('email')}
+				label="Email"
+				description="Your email address"
+				type="email"
+				placeholder="Example: john@doe.com"
+				error={errors.email?.message}
+			/>
+			<Button type="submit">Submit</Button>
+		</form>
 	)
 }
