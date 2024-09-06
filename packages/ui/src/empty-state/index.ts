6a5cd6a7143328f42872757a3f2a16806e52aa0f
Daren Malfait <daren@nerdfish.be>
2024-09-06 10:16:48 +0200

    feat(empty-state): add new component

---
+++ packages/ui/src/empty-state/index.ts
@@ -0,0 +1,17 @@
+export {
+	EmptyStateRoot,
+	EmptyStateTitle,
+	EmptyStateIcon,
+	EmptyStateDescription,
+	EmptyStateActions,
+} from './empty-state'
+
+export type {
+	EmptyStateRootProps,
+	EmptyStateTitleProps,
+	EmptyStateIconProps,
+	EmptyStateDescriptionProps,
+	EmptyStateActionsProps,
+} from './empty-state'
+
+export * as EmptyState from './namespace'
