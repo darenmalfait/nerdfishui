84db30363efcd8ccc61283e9958c172d8d5fe127
Daren Malfait <daren@nerdfish.be>
2024-09-12 15:30:19 +0200

    feat(ui): add resizable component

---
+++ packages/ui/src/resizable/index.ts
@@ -0,0 +1,9 @@
+export { ResizableRoot, ResizablePanel, ResizableHandle } from './resizable'
+
+export type {
+	ResizableRootProps,
+	ResizablePanelProps,
+	ResizableHandleProps,
+} from './resizable'
+
+export * as Resizable from './namespace'
