22da3df485cd299eb69856c87da89e299de0b9ba
Daren Malfait <daren@nerdfish.be>
2024-09-13 14:25:47 +0200

    style(docs): reduce max size

--- apps/docs/src/components/site-header.tsx
+++ apps/docs/src/components/site-header.tsx
@@ -4,7 +4,7 @@ import { ThemeToggle } from './theme-toggle'
 export function SiteHeader() {
 	return (
 		<header className="shadow-outline bg-primary sticky top-0 z-40 w-full">
-			<div className="container mx-auto flex h-16 items-center space-x-4 px-4 sm:justify-between sm:space-x-0">
+			<div className="container mx-auto flex h-16 max-w-screen-2xl items-center space-x-4 px-4 sm:justify-between sm:space-x-0">
 				<MainNavigation />
 				<div className="flex flex-1 items-center justify-end space-x-4">
 					<nav className="flex items-center space-x-1">
