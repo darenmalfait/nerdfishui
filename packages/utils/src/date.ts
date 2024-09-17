f93e9e8b68e9293da71715548438b884ab2121a6
Daren Malfait <daren@nerdfish.be>
2024-09-17 21:21:35 +0200

    chore(utils): remove time utility

--- packages/utils/src/date.ts
+++ packages/utils/src/date.ts
@@ -1,5 +1,3 @@
-import { CalendarDateTime } from '@internationalized/date'
-
 export function roundToNearest15(date = new Date()) {
 	const minutes = 15
 	const ms = 1000 * 60 * minutes
@@ -7,18 +5,3 @@ export function roundToNearest15(date = new Date()) {
 	// 👇️ replace Math.round with Math.ceil to always round UP
 	return new Date(Math.round(date.getTime() / ms) * ms)
 }
-
-export function dateToCalendarDateTime(date: Date): CalendarDateTime {
-	return new CalendarDateTime(
-		date.getFullYear(),
-		date.getMonth() + 1, // JavaScript months are 0-based
-		date.getDate(),
-		date.getHours(),
-		date.getMinutes(),
-		date.getSeconds(),
-		date.getMilliseconds(),
-	)
-}
-
-export { CalendarDateTime } from '@internationalized/date'
-export { Time } from '@internationalized/date'
