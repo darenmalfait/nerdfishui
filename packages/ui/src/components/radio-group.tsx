74c99b838d9a84146c31cabbb2cfc866a1d0786e
Daren Malfait <daren@nerdfish.be>
2024-09-24 21:58:56 +0200

    fix(radio-group): improve click area

--- packages/ui/src/components/radio-group.tsx
+++ packages/ui/src/components/radio-group.tsx
@@ -31,13 +31,13 @@ export function RadioGroupField({
 				className,
 
 				// Base layout
-				'grid grid-cols-[1.125rem_1fr] items-center gap-x-4 gap-y-1 sm:grid-cols-[1rem_1fr]',
+				'grid grid-cols-[1.125rem_1fr] items-center gap-y-1 sm:grid-cols-[1rem_1fr]',
 
 				// Control layout
 				'[&>[data-slot=control]]:col-start-1 [&>[data-slot=control]]:row-start-1 [&>[data-slot=control]]:justify-self-center',
 
 				// Label layout
-				'[&>[data-slot=label]]:col-start-2 [&>[data-slot=label]]:row-start-1 [&>[data-slot=label]]:justify-self-start',
+				'[&>[data-slot=label]]:col-start-2 [&>[data-slot=label]]:row-start-1 [&>[data-slot=label]]:justify-self-start [&>[data-slot=label]]:pl-4',
 
 				// Description layout
 				'[&>[data-slot=description]]:col-start-2 [&>[data-slot=description]]:row-start-2',
