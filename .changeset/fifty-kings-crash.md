16c056f29f0b63a68fae9e51fb47f45be1601107
Daren Malfait <daren@nerdfish.be>
2024-09-25 20:03:03 +0200

    feat(skeleton): use line height as default height

---
+++ .changeset/fifty-kings-crash.md
@@ -0,0 +1,5 @@
+---
+'@nerdfish/ui': patch
+---
+
+improve `Skeleton`component to match line height of parent
--- apps/docs/src/app/docs/components/skeleton/example.tsx
+++ apps/docs/src/app/docs/components/skeleton/example.tsx
@@ -1,15 +1,44 @@
 'use client'
 
-import { Skeleton } from '@nerdfish/ui'
+import {
+	Button,
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+	H2,
+	Paragraph,
+	Skeleton,
+} from '@nerdfish/ui'
+import * as React from 'react'
 
 export function SkeletonExample() {
+	const [loading, setLoading] = React.useState(true)
+
 	return (
-		<div className="flex items-center space-x-4">
-			<Skeleton className="size-12 rounded-full" />
-			<div className="space-y-2">
-				<Skeleton className="h-4 w-[250px]" />
-				<Skeleton className="h-4 w-[200px]" />
-			</div>
+		<div className="flex flex-col items-center space-y-4">
+			<Button onClick={() => setLoading(!loading)}>Toggle</Button>
+
+			<Card className="w-[350px]">
+				<CardHeader>
+					<CardTitle>
+						<H2>{loading ? <Skeleton className="w-[250px]" /> : 'Title'}</H2>
+					</CardTitle>
+					<CardDescription className="text-sm">
+						{loading ? <Skeleton className="w-[200px]" /> : 'Description'}
+					</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<Paragraph>
+						{loading ? (
+							<Skeleton count={4} />
+						) : (
+							'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+						)}
+					</Paragraph>
+				</CardContent>
+			</Card>
 		</div>
 	)
 }
