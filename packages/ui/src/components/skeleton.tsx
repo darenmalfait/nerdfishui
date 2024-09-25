4f746681406a74b83da206d499d1c17155d51461
Daren Malfait <daren@nerdfish.be>
2024-09-25 20:03:03 +0200

    feat(skeleton): use line height as default height

--- packages/ui/src/components/skeleton.tsx
+++ packages/ui/src/components/skeleton.tsx
@@ -3,43 +3,25 @@
 import { cx } from '@nerdfish/utils'
 import * as React from 'react'
 
-import { Card, CardHeader, CardContent, CardFooter } from './card'
-
 export function Skeleton({
 	className,
+	count = 1,
 	...props
-}: React.HTMLAttributes<HTMLDivElement>) {
+}: React.HTMLAttributes<HTMLDivElement> & {
+	count?: number
+}) {
 	return (
-		<div
-			className={cx('bg-inverted/10 animate-pulse rounded-lg', className)}
-			{...props}
-		/>
+		<>
+			{Array.from({ length: count }).map((_, index) => (
+				<span
+					{...props}
+					key={index}
+					className={cx(
+						'bg-inverted/10 inline-block h-[1lh] w-full animate-pulse rounded-lg text-inherit',
+						className,
+					)}
+				/>
+			))}
+		</>
 	)
-}
-
-export function CardSkeleton({ className }: { className?: string }) {
-	return (
-		<Card className={className}>
-			<CardHeader className="flex flex-col gap-2">
-				<Skeleton className="h-5 w-1/5" />
-				<Skeleton className="h-4 w-4/5" />
-			</CardHeader>
-			<CardContent className="h-10" />
-			<CardFooter>
-				<Skeleton className="h-8 w-[120px]" />
-			</CardFooter>
-		</Card>
-	)
-}
-
-export function H1Skeleton({ className }: { className?: string }) {
-	return <Skeleton className={cx('h-12 w-1/3 rounded-lg', className)} />
-}
-
-export function H2Skeleton({ className }: { className?: string }) {
-	return <Skeleton className={cx('h-10 w-1/3 rounded-lg', className)} />
-}
-
-export function ButtonSkeleton({ className }: { className?: string }) {
-	return <Skeleton className={cx('h-4 w-16 rounded-lg', className)} />
 }
