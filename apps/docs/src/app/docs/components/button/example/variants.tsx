8f06d7462c2812c76ef6fada0ba6bbfc71857b21
Daren Malfait <daren@nerdfish.be>
2024-09-27 12:00:26 +0200

    feat(ui): move accentuate into variant

--- apps/docs/src/app/docs/components/button/example/variants.tsx
+++ apps/docs/src/app/docs/components/button/example/variants.tsx
@@ -14,6 +14,7 @@ export function ButtonVariantsExample() {
 			<Button variant="success">Success</Button>
 			<Button variant="ghost">Ghost</Button>
 			<Button variant="outline">Outline</Button>
+			<Button variant="accentuate">Accentuate</Button>
 			<Button variant="link">Link</Button>
 		</div>
 	)
