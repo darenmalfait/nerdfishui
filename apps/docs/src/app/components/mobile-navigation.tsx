cd2db28ff52f45ba51036ef01baec6dd8b77e97c
Daren Malfait <daren@nerdfish.be>
2024-10-21 10:36:48 +0200

    fix(drawer): hide handles

--- apps/docs/src/app/components/mobile-navigation.tsx
+++ apps/docs/src/app/components/mobile-navigation.tsx
@@ -3,11 +3,15 @@
 import {
 	Button,
 	Drawer,
+	DrawerClose,
 	DrawerContent,
+	DrawerHeader,
 	DrawerOverlay,
 	NavigationList,
 	NavigationListItem,
+	Separator,
 } from '@nerdfish/ui'
+import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { siteConfig } from 'site.config'
 import { useMobileNav } from '../mobile-nav-provider'
@@ -16,6 +20,7 @@ import {
 	GettingStartedNavigation,
 } from './docs-navigation'
 import { Icons } from './icons'
+import { Logo } from './icons/logo'
 import { stripTrailingSlash } from '~/lib/utils/string'
 
 export function MobileNavigation() {
@@ -42,7 +47,28 @@ export function MobileNavigation() {
 				}}
 			>
 				<DrawerOverlay />
-				<DrawerContent className="!w-full max-w-[300px] px-4">
+				<DrawerContent hideCloseButton className="!w-full max-w-[300px] px-4">
+					<DrawerHeader>
+						<div className="flex flex-row items-center justify-between gap-4">
+							<Link href="/" aria-label="Home">
+								<div className="flex items-center space-x-3">
+									<Logo className="h-6" />
+									<span className="text-primary ml-2 flex items-center space-x-2 text-lg font-semibold">
+										Nerdfish
+										<span className="border-muted ml-1 rounded-lg border p-1 text-[8px] leading-snug">
+											UI
+										</span>
+									</span>
+								</div>
+							</Link>
+							<DrawerClose className="-mr-4">
+								<Button variant="ghost" size="icon">
+									<Icons.Close className="size-4" />
+								</Button>
+							</DrawerClose>
+						</div>
+					</DrawerHeader>
+					<Separator />
 					<NavigationList>
 						{siteConfig.mainNav.length
 							? siteConfig.mainNav.map((item) => (
