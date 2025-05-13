50925223f52a4c6d03c16bd8839226ba8dd994f6
Daren Malfait <daren@nerdfish.be>
2025-05-13 19:28:04 +0200

    feat(ui): remove default delay duration from tooltip

---
+++ .changeset/young-clowns-send.md
@@ -0,0 +1,5 @@
+---
+'@nerdfish/ui': patch
+---
+
+Tooltip: remove default delay
--- packages/ui/src/components/tooltip.tsx
+++ packages/ui/src/components/tooltip.tsx
@@ -4,28 +4,36 @@ import { cx } from '@nerdfish/utils'
 import * as TooltipPrimitive from '@radix-ui/react-tooltip'
 import * as React from 'react'
 
+export type TooltipProviderProps = React.ComponentProps<
+	typeof TooltipPrimitive.Provider
+>
 export const TooltipProvider = TooltipPrimitive.Provider
 
-export const Tooltip = TooltipPrimitive.Root
+export type TooltipProps = React.ComponentProps<typeof TooltipPrimitive.Root>
+export function Tooltip({ delayDuration = 100, ...props }: TooltipProps) {
+	return <TooltipPrimitive.Root delayDuration={delayDuration} {...props} />
+}
+export type TooltipTriggerProps = React.ComponentProps<
+	typeof TooltipPrimitive.Trigger
+>
 export const TooltipTrigger = TooltipPrimitive.Trigger
 
-export const TooltipContent = React.forwardRef<
-	React.ElementRef<typeof TooltipPrimitive.Content>,
-	React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>
->(({ className, sideOffset = 4, ...props }, ref) => (
-	<TooltipPrimitive.Content
-		ref={ref}
-		sideOffset={sideOffset}
-		className={cx(
-			'fade-in-50 data-[side=bottom]:slide-in-from-top-1 data-[side=left]:slide-in-from-right-1 data-[side=right]:slide-in-from-left-1 data-[side=top]:slide-in-from-bottom-1 animate-in rounded-base bg-background-muted p-sm text-foreground shadow-outline shadow-soft-xl z-50 overflow-hidden text-sm',
-			className,
-		)}
-		{...props}
-	/>
-))
-TooltipContent.displayName = TooltipPrimitive.Content.displayName
-
-export type TooltipProps = React.ComponentProps<typeof Tooltip>
-export type TooltipTriggerProps = React.ComponentProps<typeof TooltipTrigger>
-export type TooltipContentProps = React.ComponentProps<typeof TooltipContent>
-export type TooltipProviderProps = React.ComponentProps<typeof TooltipProvider>
+export type TooltipContentProps = React.ComponentProps<
+	typeof TooltipPrimitive.Content
+>
+export function TooltipContent({
+	className,
+	sideOffset = 4,
+	...props
+}: TooltipContentProps) {
+	return (
+		<TooltipPrimitive.Content
+			sideOffset={sideOffset}
+			className={cx(
+				'fade-in-50 data-[side=bottom]:slide-in-from-top-1 data-[side=left]:slide-in-from-right-1 data-[side=right]:slide-in-from-left-1 data-[side=top]:slide-in-from-bottom-1 animate-in rounded-base bg-background-muted p-sm text-foreground shadow-outline shadow-soft-xl z-50 overflow-hidden text-sm',
+				className,
+			)}
+			{...props}
+		/>
+	)
+}
