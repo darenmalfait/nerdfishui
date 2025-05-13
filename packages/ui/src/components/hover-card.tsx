60d5cd06977f36ab55fc5aebcdf97a116e7a05c7
Daren Malfait <daren@nerdfish.be>
2025-05-13 19:24:09 +0200

    refactor(hover-card): remove forwardref

--- packages/ui/src/components/hover-card.tsx
+++ packages/ui/src/components/hover-card.tsx
@@ -4,32 +4,34 @@ import { cx } from '@nerdfish/utils'
 import * as HoverCardPrimitive from '@radix-ui/react-hover-card'
 import * as React from 'react'
 
-export const HoverCard = HoverCardPrimitive.Root
-export const HoverCardTrigger = HoverCardPrimitive.Trigger
-
-export const HoverCardContent = React.forwardRef<
-	React.ElementRef<typeof HoverCardPrimitive.Content>,
-	React.ComponentPropsWithoutRef<typeof HoverCardPrimitive.Content>
->(({ className, align = 'center', sideOffset = 4, ...props }, ref) => (
-	<HoverCardPrimitive.Content
-		ref={ref}
-		align={align}
-		sideOffset={sideOffset}
-		className={cx(
-			'zoom-in-90 animate-in rounded-base bg-background-muted p-md shadow-outline shadow-soft-xl z-50 w-64 outline-none',
-			className,
-		)}
-		{...props}
-	/>
-))
-HoverCardContent.displayName = HoverCardPrimitive.Content.displayName
-
-export type HoverCardProps = React.ComponentPropsWithoutRef<
+export type HoverCardProps = React.ComponentProps<
 	typeof HoverCardPrimitive.Root
 >
-export type HoverCardTriggerProps = React.ComponentPropsWithoutRef<
+export const HoverCard = HoverCardPrimitive.Root
+
+export type HoverCardTriggerProps = React.ComponentProps<
 	typeof HoverCardPrimitive.Trigger
 >
-export type HoverCardContentProps = React.ComponentPropsWithoutRef<
+export const HoverCardTrigger = HoverCardPrimitive.Trigger
+
+export type HoverCardContentProps = React.ComponentProps<
 	typeof HoverCardPrimitive.Content
 >
+export function HoverCardContent({
+	className,
+	align = 'center',
+	sideOffset = 4,
+	...props
+}: HoverCardContentProps) {
+	return (
+		<HoverCardPrimitive.Content
+			align={align}
+			sideOffset={sideOffset}
+			className={cx(
+				'zoom-in-90 animate-in rounded-base bg-background-muted p-md shadow-outline shadow-soft-xl z-50 w-64 outline-none',
+				className,
+			)}
+			{...props}
+		/>
+	)
+}
