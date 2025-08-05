5dcab45d5de8c34137de6e28e9f208349b848e5c
Daren Malfait <daren@nerdfish.be>
2025-08-05 11:43:30 +0200

    refactor(ui): remove forwardsrefs from description and field

--- packages/ui/src/components/description.tsx
+++ packages/ui/src/components/description.tsx
@@ -1,41 +1,25 @@
 import { cx } from '@nerdfish/utils'
 import * as React from 'react'
 
-export const Description = React.forwardRef<
-	HTMLParagraphElement,
-	React.HTMLAttributes<HTMLParagraphElement>
->(({ className, ...props }, ref) => {
+export type DescriptionProps = React.ComponentProps<'p'>
+
+export function Description({ className, ...props }: DescriptionProps) {
 	return (
 		<p
-			ref={ref}
 			data-slot="description"
 			className={cx('text-foreground-muted text-sm', className)}
 			{...props}
 		/>
 	)
-})
+}
 
-Description.displayName = 'Description'
+export type ErrorDescriptionProps = React.ComponentProps<'p'>
 
-export type DescriptionProps = React.ComponentPropsWithoutRef<
-	typeof Description
->
-
-export const ErrorDescription = React.forwardRef<
-	HTMLParagraphElement,
-	React.HTMLAttributes<HTMLParagraphElement>
->(({ className, ...props }, ref) => {
+export function ErrorDescription({ className, ...props }: DescriptionProps) {
 	return (
 		<p
-			ref={ref}
 			className={cx('text-foreground-danger text-sm font-medium', className)}
 			{...props}
 		/>
 	)
-})
-
-ErrorDescription.displayName = 'ErrorDescription'
-
-export type ErrorDescriptionProps = React.ComponentPropsWithoutRef<
-	typeof ErrorDescription
->
+}
--- packages/ui/src/components/field.tsx
+++ packages/ui/src/components/field.tsx
@@ -1,13 +1,8 @@
 import { cx } from '@nerdfish/utils'
 import * as React from 'react'
 
-export const Field = React.forwardRef<
-	HTMLDivElement,
-	React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => {
-	return <div ref={ref} className={cx('space-y-sm', className)} {...props} />
-})
+export type FieldProps = React.ComponentProps<'div'>
 
-Field.displayName = 'Field'
-
-export type FieldProps = React.ComponentPropsWithoutRef<typeof Field>
+export function Field({ className, ...props }: FieldProps) {
+	return <div className={cx('space-y-sm', className)} {...props} />
+}
