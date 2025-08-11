663d2ce80eff5c1c396d2bee7af454af6aa79882
Daren Malfait <daren@nerdfish.be>
2025-08-11 15:00:49 +0200

    refactor(textarea): remove forwardref

--- packages/ui/src/components/textarea.tsx
+++ packages/ui/src/components/textarea.tsx
@@ -2,11 +2,15 @@ import { type VariantProps, cx } from '@nerdfish/utils'
 import * as React from 'react'
 import { inputVariants } from './input'
 
-export const Textarea = React.forwardRef<
-	HTMLTextAreaElement,
-	React.ComponentPropsWithoutRef<'textarea'> &
-		VariantProps<typeof inputVariants>
->(({ className, inputSize, variant, ...props }, ref) => {
+export interface TextareaProps
+	extends React.ComponentProps<'textarea'>,
+		VariantProps<typeof inputVariants> {}
+export function TextArea({
+	className,
+	inputSize,
+	variant,
+	...props
+}: TextareaProps) {
 	return (
 		<textarea
 			className={cx(
@@ -14,9 +18,7 @@ export const Textarea = React.forwardRef<
 				'focus:outline-brand relative flex min-h-[80px]',
 				className,
 			)}
-			ref={ref}
 			{...props}
 		/>
 	)
-})
-Textarea.displayName = 'Textarea'
+}
