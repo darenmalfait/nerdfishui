a7d819c12df5e374a8abbc8c25f6413d30858683
Daren Malfait <daren@nerdfish.be>
2025-08-14 15:59:21 +0200

    refactor(input-otp): remove forwardrefs

--- packages/ui/src/components/input-otp.tsx
+++ packages/ui/src/components/input-otp.tsx
@@ -11,80 +11,7 @@ import {
 } from 'input-otp'
 import { Dot } from 'lucide-react'
 import * as React from 'react'
-
 import { inputVariants } from './input'
-
-export const InputOTP = React.forwardRef<
-	React.ElementRef<typeof OTPInput>,
-	React.ComponentPropsWithoutRef<typeof OTPInput>
->(({ className, ...props }, ref) => (
-	<OTPInput
-		ref={ref}
-		containerClassName={cx('gap-md flex items-center', className)}
-		{...props}
-	/>
-))
-InputOTP.displayName = 'InputOTP'
-
-export const InputOTPGroup = React.forwardRef<
-	React.ElementRef<'div'>,
-	React.ComponentPropsWithoutRef<'div'>
->(({ className, ...props }, ref) => (
-	<div
-		ref={ref}
-		className={cx('gap-sm flex items-center', className)}
-		{...props}
-	/>
-))
-InputOTPGroup.displayName = 'InputOTPGroup'
-
-export const InputOTPSlot = React.forwardRef<
-	React.ElementRef<'div'>,
-	SlotProps &
-		React.ComponentPropsWithoutRef<'div'> & {
-			activeClassName?: string
-		}
->(
-	(
-		{ char, hasFakeCaret, isActive, className, activeClassName, ...props },
-		ref,
-	) => {
-		return (
-			<div
-				ref={ref}
-				className={cx(
-					inputVariants(),
-					'relative flex h-10 w-10 items-center justify-center',
-					isActive &&
-						cx(
-							'ring-brand ring-offset-background z-10 ring-2',
-							activeClassName,
-						),
-					className,
-				)}
-				{...props}
-			>
-				{char}
-				{hasFakeCaret ? (
-					<div className="pointer-events-none absolute inset-0 flex items-center justify-center">
-						<div className="animate-caret-blink bg-foreground h-4 w-px duration-1000" />
-					</div>
-				) : null}
-			</div>
-		)
-	},
-)
-InputOTPSlot.displayName = 'InputOTPSlot'
-
-export const InputOTPSeparator = React.forwardRef<
-	React.ElementRef<'div'>,
-	React.ComponentPropsWithoutRef<'div'>
->(({ ...props }, ref) => (
-	<div ref={ref} role="separator" {...props}>
-		<Dot />
-	</div>
-))
-InputOTPSeparator.displayName = 'InputOTPSeparator'
 
 export const inputOTPPatterns = {
 	digits: REGEXP_ONLY_DIGITS,
@@ -92,13 +19,65 @@ export const inputOTPPatterns = {
 	digitsAndChars: REGEXP_ONLY_DIGITS_AND_CHARS,
 }
 
-export type InputOTPProps = React.ComponentPropsWithoutRef<typeof InputOTP>
-export type InputOTPGroupProps = React.ComponentPropsWithoutRef<
-	typeof InputOTPGroup
->
-export type InputOTPSlotProps = React.ComponentPropsWithoutRef<
-	typeof InputOTPSlot
->
-export type InputOTPSeparatorProps = React.ComponentPropsWithoutRef<
-	typeof InputOTPSeparator
->
+export type InputOTPProps = React.ComponentProps<typeof OTPInput>
+export function InputOTP({ className, ...props }: InputOTPProps) {
+	return (
+		<OTPInput
+			containerClassName={cx('gap-md flex items-center', className)}
+			{...props}
+		/>
+	)
+}
+
+export type InputOTPGroupProps = React.ComponentProps<'div'>
+export function InputOTPGroup({ className, ...props }: InputOTPGroupProps) {
+	return (
+		<div className={cx('gap-sm flex items-center', className)} {...props} />
+	)
+}
+
+export interface InputOTPSlotProps
+	extends SlotProps,
+		React.ComponentProps<'div'> {
+	activeClassName?: string
+}
+export function InputOTPSlot({
+	char,
+	hasFakeCaret,
+	isActive,
+	className,
+	activeClassName,
+	...props
+}: InputOTPSlotProps) {
+	return (
+		<div
+			className={cx(
+				inputVariants(),
+				'relative flex h-10 w-10 items-center justify-center',
+				isActive &&
+					cx('ring-brand ring-offset-background z-10 ring-2', activeClassName),
+				className,
+			)}
+			{...props}
+		>
+			{char}
+			{hasFakeCaret ? (
+				<div className="pointer-events-none absolute inset-0 flex items-center justify-center">
+					<div className="animate-caret-blink bg-foreground h-4 w-px duration-1000" />
+				</div>
+			) : null}
+		</div>
+	)
+}
+
+export type InputOTPSeparatorProps = React.ComponentProps<'div'>
+export function InputOTPSeparator({
+	className,
+	...props
+}: InputOTPSeparatorProps) {
+	return (
+		<div role="separator" {...props}>
+			<Dot />
+		</div>
+	)
+}
